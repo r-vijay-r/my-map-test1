c5d9644514d93059b5afd6bd8428976ad4ce004d
r-vijay-r <503r.vijay@gmail.com>
2016-11-03 00:51:09 +0530

    current location added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  lat: number = 8.537074;
-  lng: number = 76.938768;
+  lat;
+  lng;
+  show=false;
+  constructor(){
+  	function success(position) {
+    let lon=document.getElementById("lon");
+    let lan=document.getElementById("lan");
+    let interval=setInterval(()=>{
+    	let longitude: number = position.coords.longitude;
+    	let latitude:number  = position.coords.latitude;
+    	if(latitude>0){
+	    	lan.innerHTML=""+latitude;
+	    	lon.innerHTML=""+longitude;
+   		}
+    },500);
+  };
+
+  function error() {
+  };
+
+
+  navigator.geolocation.getCurrentPosition(success, error);
+  	let mapInterval=setInterval(()=>{
+  		this.lat=parseFloat(document.getElementById("lan").innerHTML);
+  		this.lng=parseFloat(document.getElementById("lon").innerHTML);
+  		if(this.lat>0) {
+			this.show=true;  			
+  		}
+
+  	},500);
+  }
 }
